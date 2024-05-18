16cad865ca21317a5f84e177e975e1b9c7be87a5
Katerina <trajkovskakaterina0@gmail.com>
2024-05-18 18:05:46 +0200

    "first commit"

---
+++ index.js
@@ -0,0 +1,14 @@
+//Simple Login System
+
+let username = "admin";
+let password = "adminpass";
+
+if (username === "admin" && password === "adminpass") {
+    console.log("Welcome, admin");
+}
+else if (username === "user123" && password === "password123 ") {
+    console.log("Welcome, user123!");
+}
+else {
+    console.log("Invalid username or password.");
+}
