b93c60de72e76ca46de28588caf54961e97ec07f
Katerina <trajkovskakaterina0@gmail.com>
2024-05-18 17:49:08 +0200

    "first commit"

---
+++ index.js
@@ -0,0 +1,16 @@
+//Checking Day of the Week
+
+let dayOfTheWeek = "Friday";
+
+if (dayOfTheWeek === "Monday") {
+    console.log("Start of the work week!");
+}
+else if (dayOfTheWeek === "Wednesday") {
+    console.log("Midweek!");
+}
+else if (dayOfTheWeek === "Friday") {
+    console.log("Almost weekend!");
+}
+else {
+    console.log("Just another day.");
+}
