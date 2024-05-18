e009aad929d250ae7bff08fc7e5d4592067faedf
Katerina <trajkovskakaterina0@gmail.com>
2024-05-18 17:43:42 +0200

    "second commit"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let age = 11;
 if (age < 12) {
     console.log("Child ticket is 5$");
 }
-else if (age >= 12 && age <= 18) {
+else if (age >= 12 && age < 18) {
     console.log("Teen ticket: $7");
 }
 else if (age >= 18 && age <= 60) {
